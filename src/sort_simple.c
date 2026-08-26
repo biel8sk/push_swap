da36fbdd2b5ac3d2a98955cbcaad5a489db81b0e
Gabriel Pires Cardim Barreto <gpires-c@c3r4p3.42sp.org.br>
2026-08-25 22:53:46 -0300

    sort simple with push swap operations only

--- src/sort_simple.c
+++ src/sort_simple.c
@@ -6,7 +6,7 @@
 /*   By: gpires-c <gpires-c@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/19 19:49:27 by gpires-c          #+#    #+#             */
-/*   Updated: 2026/08/22 00:28:16 by gpires-c         ###   ########.fr       */
+/*   Updated: 2026/08/25 22:42:25 by gpires-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,46 +32,35 @@ void	print_st(t_stack *p, char stack)
 	ft_putstr_fd("\n", 1);
 }
 
+int	is_sorted(t_stack *st)
+{
+	if (!st)
+		return (0);
+	while (st->next)
+	{
+		if (st->next->content < st->content)
+			return (st->next->content < st->content);
+		st = st->next;
+	}
+	return (st->content < st->next->content);
+}
+
 void	sort_simple(t_program *p)
 {
 	t_stack	*tmp;
 
 	while (p->a)
 	{
-		p->b = find_first_b(p);
-		if (p->b == NULL || p->b->content > (p->a)->content)
+		if (p->b == NULL || p->a->content < p->b->content)
 		{
 			pb(p);
-			if (!p->a)
-				break ;
 			continue ;
 		}
-		if (p->b->next)
+		while (p->b && p->b->content < p->a->content)
 		{
-			while (p->b->next && p->b->content < p->a->content)
-			{
-				p->b = p->b->next;
-			}
-			if (!p->b->next && p->b->content < p->a->content)
-			{
-				tmp = pop_first(&p->a);
-				tmp->previous = p->b;
-				p->b->next = tmp;
-				p->b = find_first_b(p);
-				continue ;
-			}
-			tmp = pop_first(&p->a);
-			if (p->b->previous)
-				p->b->previous->next = tmp;
-			tmp->previous = p->b->previous;
-			p->b->previous = tmp;
-			tmp->next = p->b;
-			p->b = find_first_b(p);
-			continue ;
+			pa(p);
+			sa(p);
 		}
-		tmp = pop_first(&p->a);
-		tmp->previous = p->b;
-		p->b->next = tmp;
 	}
 	tmp = p->b;
 	p->b = p->a;
